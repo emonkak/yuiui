41b957e30b86c8d6f8bd336d2e971c60455e5c45
Shota Nozaki <emonkak@gmail.com>
2021-07-23 22:37:05 +0900

    Improve the derive macro for WidgetMeta

--- rust-ui-derive/src/lib.rs
+++ rust-ui-derive/src/lib.rs
@@ -6,11 +6,23 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(WidgetMeta)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
-        impl WidgetMeta for #ident {
-            fn as_any(&self) -> &::std::any::Any {
-                self
+    let ast: DeriveInput = parse_macro_input!(input);
+    let name = ast.ident;
+    let output = if !ast.generics.params.is_empty() {
+        let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+        quote! {
+            impl #impl_generics WidgetMeta for #name #ty_generics #where_clause {
+                fn as_any(&self) -> &::std::any::Any {
+                    self
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl WidgetMeta for #name {
+                fn as_any(&self) -> &::std::any::Any {
+                    self
+                }
             }
         }
     };
