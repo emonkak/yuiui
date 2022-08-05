4e2d2c0896cd49740c974f7d013918ade8e6fb00
Shota Nozaki <emonkak@gmail.com>
2022-08-05 20:58:01 +0900

    fix: remove Pin from two worlds

--- yuiui-experiment/src/real_world.rs
+++ yuiui-experiment/src/real_world.rs
@@ -1,22 +1,20 @@
-use std::pin::Pin;
-
 use crate::element::Element;
 use crate::view::View;
 use crate::widget::WidgetPod;
 
 pub struct RealWorld<E: Element> {
-    widget_pod: Pin<Box<WidgetPod<<E::View as View>::Widget>>>,
+    widget_pod: WidgetPod<<E::View as View>::Widget>,
 }
 
 impl<E: Element> RealWorld<E> {
     pub fn new(widget_pod: WidgetPod<<E::View as View>::Widget>) -> Self {
         Self {
-            widget_pod: Box::pin(widget_pod)
+            widget_pod,
         }
     }
 
     pub fn widget_pod(&self) -> &WidgetPod<<E::View as View>::Widget> {
-        Pin::get_ref(self.widget_pod.as_ref())
+        &self.widget_pod
     }
 }
 
--- yuiui-experiment/src/virtual_world.rs
+++ yuiui-experiment/src/virtual_world.rs
@@ -1,28 +1,26 @@
-use std::pin::Pin;
-
 use crate::context::Context;
 use crate::element::Element;
 use crate::real_world::RealWorld;
 use crate::view::ViewPod;
 
 pub struct VirtualWorld<E: Element> {
-    view_pod: Pin<Box<ViewPod<E::View, E::Components>>>,
+    view_pod: ViewPod<E::View, E::Components>,
     context: Context,
 }
 
 impl<E: Element> VirtualWorld<E> {
     pub fn new(element: E) -> Self {
         let mut context = Context::new(E::depth());
-        let view_pod = Box::pin(element.build(&mut context));
+        let view_pod = element.build(&mut context);
         Self { view_pod, context }
     }
 
     pub fn realize(&self) -> RealWorld<E> {
-        let widget_pod = E::compile(&self.view_pod());
+        let widget_pod = E::compile(&self.view_pod);
         RealWorld::new(widget_pod)
     }
 
     pub fn view_pod(&self) -> &ViewPod<E::View, E::Components> {
-        Pin::get_ref(self.view_pod.as_ref())
+        &self.view_pod
     }
 }
