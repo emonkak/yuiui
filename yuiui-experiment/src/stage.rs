db800c4641857a3fd8d4bcfaee1c4239ccedd628
Shota Nozaki <emonkak@gmail.com>
2022-08-07 09:01:49 +0900

    feat(stage): add Stage::update()

--- yuiui-experiment/src/stage.rs
+++ yuiui-experiment/src/stage.rs
@@ -2,7 +2,6 @@ use crate::context::Context;
 use crate::element::Element;
 use crate::widget::WidgetNode;
 
-#[allow(dead_code)]
 pub struct Stage<E: Element> {
     node: WidgetNode<E::View, E::Components>,
     context: Context,
@@ -13,6 +12,10 @@ impl<E: Element> Stage<E> {
         let mut context = Context::new();
         let node = element.build(&mut context);
         Self { node, context }
+    }
+
+    pub fn update(&mut self, element: E) {
+        element.rebuild(self.node.scope(), &mut self.context);
     }
 
     pub fn node(&self) -> &WidgetNode<E::View, E::Components> {
