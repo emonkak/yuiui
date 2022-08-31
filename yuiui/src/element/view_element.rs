2ad3c24d777a2c672540d86332a42d379d019a0a
Shota Nozaki <emonkak@gmail.com>
2022-08-31 15:36:52 +0900

    refactor: ViewElement

--- yuiui/src/element/view_element.rs
+++ yuiui/src/element/view_element.rs
@@ -54,9 +54,11 @@ where
     ) -> bool {
         *scope.state = match scope.state.take().unwrap() {
             WidgetState::Uninitialized(_) => WidgetState::Uninitialized(self.view),
-            WidgetState::Prepared(widget, view) => WidgetState::Pending(widget, view, self.view),
-            WidgetState::Dirty(widget, view) => WidgetState::Pending(widget, view, self.view),
-            WidgetState::Pending(widget, view, _) => WidgetState::Pending(widget, view, self.view),
+            WidgetState::Prepared(widget, view)
+            | WidgetState::Dirty(widget, view)
+            | WidgetState::Pending(widget, view, _) => {
+                WidgetState::Pending(widget, view, self.view)
+            }
         }
         .into();
         *scope.dirty = true;
