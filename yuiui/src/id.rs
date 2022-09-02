7d4c07d5cfaf407f7f0034ab90738c7d898ef75f
Shota Nozaki <emonkak@gmail.com>
2022-09-02 19:34:56 +0900

    perf: optimize RenderLoop

--- yuiui/src/id.rs
+++ yuiui/src/id.rs
@@ -1,3 +1,6 @@
+use std::collections::vec_deque::IntoIter;
+use std::collections::VecDeque;
+
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Id(pub(crate) u64);
 
@@ -5,7 +8,7 @@ impl Id {
     pub const ROOT: Self = Self(0);
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct IdPath {
     path: Vec<Id>,
 }
@@ -15,12 +18,12 @@ impl IdPath {
         Self { path: Vec::new() }
     }
 
-    pub fn bottom_id(&self) -> Id {
-        self.path.last().copied().unwrap_or(Id::ROOT)
-    }
-
     pub fn top_id(&self) -> Id {
         self.path.first().copied().unwrap_or(Id::ROOT)
+    }
+
+    pub fn bottom_id(&self) -> Id {
+        self.path.last().copied().unwrap_or(Id::ROOT)
     }
 
     pub fn starts_with(&self, needle: &Self) -> bool {
@@ -34,6 +37,166 @@ impl IdPath {
     pub fn pop(&mut self) -> Id {
         self.path.pop().unwrap()
     }
+
+    fn strip_intersection<'other>(&self, other: &'other Self) -> (&[Id], &'other [Id]) {
+        let mismatched_index = match self.path.iter().zip(&other.path).position(|(x, y)| x != y) {
+            None => self.path.len().min(other.path.len()),
+            Some(diff) => diff,
+        };
+
+        let left = &self.path[mismatched_index..];
+        let right = &other.path[mismatched_index..];
+
+        (left, right)
+    }
+}
+
+impl FromIterator<Id> for IdPath {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = Id>,
+    {
+        Self {
+            path: Vec::from_iter(iter),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct IdSelection {
+    items: VecDeque<(IdPath, ComponentIndex)>,
+}
+
+impl IdSelection {
+    pub fn new() -> Self {
+        Self {
+            items: VecDeque::new(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn select(&mut self, id_path: IdPath, component_index: ComponentIndex) {
+        let mut should_push = true;
+        self.items
+            .retain_mut(|(selection_id_path, selection_component_index)| {
+                match selection_id_path.strip_intersection(&id_path) {
+                    ([_, ..], [_, ..]) => true,
+                    ([], [_, ..]) => {
+                        should_push = false;
+                        true
+                    }
+                    ([_, ..], []) => false,
+                    ([], []) => {
+                        should_push = false;
+                        *selection_component_index =
+                            (*selection_component_index).min(component_index);
+                        true
+                    }
+                }
+            });
+        if should_push {
+            self.items.push_back((id_path, component_index));
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<(IdPath, ComponentIndex)> {
+        self.items.pop_front()
+    }
+}
+
+impl IntoIterator for IdSelection {
+    type Item = (IdPath, ComponentIndex);
+
+    type IntoIter = IntoIter<(IdPath, ComponentIndex)>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
+    }
 }
 
 pub type ComponentIndex = usize;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_id_selection() {
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([]), 0);
+        id_selection.select(IdPath::from_iter([]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([]), 1);
+        id_selection.select(IdPath::from_iter([]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([]), 0);
+        id_selection.select(IdPath::from_iter([]), 1);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0)]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0)]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0)]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 1);
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0), Id(1)]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 1);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0), Id(1)]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0), Id(2)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0)]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0)]), 0)]
+        );
+
+        let mut id_selection = IdSelection::new();
+        id_selection.select(IdPath::from_iter([Id(0)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0), Id(1)]), 0);
+        id_selection.select(IdPath::from_iter([Id(0), Id(2)]), 0);
+        assert_eq!(
+            id_selection.into_iter().collect::<Vec<_>>(),
+            vec![(IdPath::from_iter([Id(0)]), 0)]
+        );
+    }
+}
--- yuiui/src/render_loop.rs
+++ yuiui/src/render_loop.rs
@@ -7,7 +7,7 @@ use crate::command::Command;
 use crate::context::{EffectContext, RenderContext};
 use crate::effect::{Effect, EffectPath};
 use crate::element::{Element, ElementSeq};
-use crate::id::{ComponentIndex, IdPath};
+use crate::id::IdSelection;
 use crate::state::State;
 use crate::view::View;
 use crate::widget_node::{CommitMode, WidgetNode, WidgetNodeSeq};
@@ -18,8 +18,8 @@ pub struct RenderLoop<El: Element<S, E>, S: State, E> {
     env: E,
     render_context: RenderContext,
     effect_queue: VecDeque<(EffectPath, Effect<S>)>,
-    update_queue: VecDeque<(IdPath, ComponentIndex)>,
-    commit_queue: VecDeque<(IdPath, ComponentIndex)>,
+    update_selection: IdSelection,
+    commit_selection: IdSelection,
     is_mounted: bool,
 }
 
@@ -38,15 +38,15 @@ where
             env,
             render_context: RenderContext::new(),
             effect_queue: VecDeque::new(),
-            update_queue: VecDeque::new(),
-            commit_queue: VecDeque::new(),
+            update_selection: IdSelection::new(),
+            commit_selection: IdSelection::new(),
             is_mounted: false,
         }
     }
 
     pub fn run(&mut self, deadline: &Instant) {
         if deadline_did_timeout(&deadline) {
-            self.request_render();
+            self.schedule_render();
             return;
         }
 
@@ -54,12 +54,12 @@ where
             while let Some((path, effect)) = self.effect_queue.pop_front() {
                 self.apply_effect(path, effect);
                 if deadline_did_timeout(&deadline) {
-                    self.request_render();
+                    self.schedule_render();
                     return;
                 }
             }
 
-            while let Some((id_path, component_index)) = self.update_queue.pop_front() {
+            while let Some((id_path, component_index)) = self.update_selection.pop() {
                 self.node.update_subtree(
                     &id_path,
                     component_index,
@@ -68,16 +68,16 @@ where
                     &mut self.render_context,
                 );
                 if self.is_mounted {
-                    self.commit_queue.push_back((id_path, component_index));
+                    self.commit_selection.select(id_path, component_index);
                 }
                 if deadline_did_timeout(&deadline) {
-                    self.request_render();
+                    self.schedule_render();
                     return;
                 }
             }
 
             if self.is_mounted {
-                while let Some((id_path, component_index)) = self.commit_queue.pop_front() {
+                while let Some((id_path, component_index)) = self.commit_selection.pop() {
                     let mut effect_context = EffectContext::new();
                     self.node.commit_subtree(
                         &id_path,
@@ -88,7 +88,7 @@ where
                     );
                     self.effect_queue.extend(effect_context.into_effects());
                     if deadline_did_timeout(&deadline) {
-                        self.request_render();
+                        self.schedule_render();
                         return;
                     }
                 }
@@ -103,7 +103,7 @@ where
                 self.effect_queue.extend(effect_context.into_effects());
                 self.is_mounted = true;
                 if deadline_did_timeout(&deadline) {
-                    self.request_render();
+                    self.schedule_render();
                     return;
                 }
             }
@@ -118,18 +118,28 @@ where
         self.effect_queue.push_back((effect_path, effect));
     }
 
+    fn schedule_render(&self) {
+        let is_finished = self.effect_queue.is_empty()
+            && self.update_selection.is_empty()
+            && self.commit_selection.is_empty()
+            && self.is_mounted;
+        if !is_finished {
+            self.env.request_render();
+        }
+    }
+
     fn apply_effect(&mut self, effect_path: EffectPath, effect: Effect<S>) {
         match effect {
             Effect::Message(message) => {
                 if self.state.reduce(message) {
-                    self.update_queue
-                        .push_back((effect_path.state_id_path, effect_path.state_component_index));
+                    self.update_selection
+                        .select(effect_path.state_id_path, effect_path.state_component_index);
                 }
             }
             Effect::Mutation(mutation) => {
                 if mutation(&mut self.state) {
-                    self.update_queue
-                        .push_back((effect_path.state_id_path, effect_path.state_component_index));
+                    self.update_selection
+                        .select(effect_path.state_id_path, effect_path.state_component_index);
                 }
             }
             Effect::Command(command, cancellation_token) => {
@@ -144,24 +154,19 @@ where
             }
             Effect::InternalEvent(event) => {
                 let mut effect_context = EffectContext::new();
-                self.node
-                    .internal_event(&event, &effect_path.id_path, &self.state, &self.env, &mut effect_context);
+                self.node.internal_event(
+                    &event,
+                    &effect_path.id_path,
+                    &self.state,
+                    &self.env,
+                    &mut effect_context,
+                );
                 self.effect_queue.extend(effect_context.into_effects());
             }
             Effect::RequestUpdate => {
-                self.update_queue
-                    .push_front((effect_path.id_path, effect_path.component_index));
+                self.update_selection
+                    .select(effect_path.state_id_path, effect_path.state_component_index);
             }
-        }
-    }
-
-    fn request_render(&self) {
-        let is_finished = self.effect_queue.is_empty()
-            && self.update_queue.is_empty()
-            && self.commit_queue.is_empty()
-            && self.is_mounted;
-        if !is_finished {
-            self.env.request_render();
         }
     }
 }
@@ -184,8 +189,8 @@ where
             .field("env", &self.env)
             .field("render_context", &self.render_context)
             .field("effect_queue", &self.effect_queue)
-            .field("update_queue", &self.update_queue)
-            .field("commit_queue", &self.commit_queue)
+            .field("update_selection", &self.update_selection)
+            .field("commit_selection", &self.commit_selection)
             .field("is_mounted", &self.is_mounted)
             .finish()
     }
