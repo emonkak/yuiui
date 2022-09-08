71875fb4276c5e610ca180de871dd3b1b5c89b65
Shota Nozaki <emonkak@gmail.com>
2022-09-08 14:26:13 +0900

    style: fix the definition of RenderFlags

--- yuiui/src/storages/mod.rs
+++ yuiui/src/storages/mod.rs
@@ -8,7 +8,7 @@ use bitflags::bitflags;
 
 bitflags! {
     pub struct RenderFlags: u32 {
-        const NONE = 0b000;
+        const NONE     = 0b000;
         const COMMITED = 0b001;
         const UPDATED  = 0b010;
         const SWAPPED  = 0b100;
