4136b0c8d3b07bba3a8abe25029f47c24fcad1d2
Shota Nozaki <emonkak@gmail.com>
2022-09-10 11:59:32 +0900

    refactor: ComponentStack

--- yuiui/src/component_stack.rs
+++ yuiui/src/component_stack.rs
@@ -14,21 +14,21 @@ pub trait ComponentStack<S, M, B>: Sized {
 
     type View: View<S, M, B>;
 
-    fn update<'a>(
-        node: &mut ViewNodeMut<'a, Self::View, Self, S, M, B>,
-        target_depth: Depth,
-        current_depth: Depth,
-        context: &mut RenderContext,
-        store: &Store<S>,
-        backend: &mut B,
-    ) -> bool;
-
     fn commit(
         &mut self,
         mode: CommitMode,
         target_depth: Depth,
         current_depth: Depth,
         context: &mut MessageContext<M>,
+        store: &Store<S>,
+        backend: &mut B,
+    ) -> bool;
+
+    fn update<'a>(
+        node: &mut ViewNodeMut<'a, Self::View, Self, S, M, B>,
+        target_depth: Depth,
+        current_depth: Depth,
+        context: &mut RenderContext,
         store: &Store<S>,
         backend: &mut B,
     ) -> bool;
@@ -43,6 +43,29 @@ where
     const LEN: usize = 1 + CS::LEN;
 
     type View = <C::Element as Element<S, M, B>>::View;
+
+    fn commit(
+        &mut self,
+        mode: CommitMode,
+        target_depth: Depth,
+        current_depth: Depth,
+        context: &mut MessageContext<M>,
+        store: &Store<S>,
+        backend: &mut B,
+    ) -> bool {
+        if target_depth <= current_depth {
+            self.0.commit(mode, current_depth, context, store, backend)
+        } else {
+            self.1.commit(
+                mode,
+                target_depth,
+                current_depth + 1,
+                context,
+                store,
+                backend,
+            )
+        }
+    }
 
     fn update<'a>(
         node: &mut ViewNodeMut<'a, Self::View, Self, S, M, B>,
@@ -75,29 +98,6 @@ where
             )
         }
     }
-
-    fn commit(
-        &mut self,
-        mode: CommitMode,
-        target_depth: Depth,
-        current_depth: Depth,
-        context: &mut MessageContext<M>,
-        store: &Store<S>,
-        backend: &mut B,
-    ) -> bool {
-        if target_depth <= current_depth {
-            self.0.commit(mode, current_depth, context, store, backend)
-        } else {
-            self.1.commit(
-                mode,
-                target_depth,
-                current_depth + 1,
-                context,
-                store,
-                backend,
-            )
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -114,23 +114,23 @@ impl<V: View<S, M, B>, S, M, B> ComponentStack<S, M, B> for ComponentEnd<V> {
 
     type View = V;
 
-    fn update<'a>(
-        _node: &mut ViewNodeMut<'a, V, Self, S, M, B>,
-        _target_depth: Depth,
-        _current_depth: Depth,
-        _context: &mut RenderContext,
-        _store: &Store<S>,
-        _backend: &mut B,
-    ) -> bool {
-        false
-    }
-
     fn commit(
         &mut self,
         _mode: CommitMode,
         _target_depth: Depth,
         _current_depth: Depth,
         _context: &mut MessageContext<M>,
+        _store: &Store<S>,
+        _backend: &mut B,
+    ) -> bool {
+        false
+    }
+
+    fn update<'a>(
+        _node: &mut ViewNodeMut<'a, V, Self, S, M, B>,
+        _target_depth: Depth,
+        _current_depth: Depth,
+        _context: &mut RenderContext,
         _store: &Store<S>,
         _backend: &mut B,
     ) -> bool {
