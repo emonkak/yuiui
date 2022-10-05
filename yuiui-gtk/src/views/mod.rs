88a2190e0077cc1e4cf62d24115081c1407d2787
Shota Nozaki <emonkak@gmail.com>
2022-10-05 10:48:22 +0900

    feat(gtk): implement Notebook

--- yuiui-gtk/src/views/mod.rs
+++ yuiui-gtk/src/views/mod.rs
@@ -6,6 +6,7 @@ mod flow_box;
 mod grid;
 mod label;
 mod list_box;
+mod notebook;
 mod stack;
 
 pub use button::Button;
@@ -15,5 +16,6 @@ pub use flow_box::{FlowBox, FlowBoxChild};
 pub use grid::{Grid, GridCell, GridChild};
 pub use label::Label;
 pub use list_box::ListBox;
+pub use notebook::Notebook;
 pub use r#box::Box;
 pub use stack::{Stack, StackPage, StackSwitcher};
---
+++ yuiui-gtk/src/views/notebook.rs
@@ -0,0 +1,260 @@
+use gtk::{gdk, glib, prelude::*};
+use std::marker::PhantomData;
+use yuiui::{
+    ComponentStack, ElementSeq, EventListener, Lifecycle, MessageContext, Store, Traversable, View,
+    ViewNode, ViewNodeSeq, Visitor,
+};
+use yuiui_gtk_derive::WidgetBuilder;
+
+#[derive(Clone, Debug, WidgetBuilder)]
+#[widget(gtk::Notebook)]
+pub struct Notebook<Children> {
+    enable_popup: Option<bool>,
+    group_name: Option<String>,
+    page: Option<i32>,
+    scrollable: Option<bool>,
+    show_border: Option<bool>,
+    show_tabs: Option<bool>,
+    tab_pos: Option<gtk::PositionType>,
+    can_focus: Option<bool>,
+    can_target: Option<bool>,
+    css_classes: Option<Vec<String>>,
+    css_name: Option<String>,
+    cursor: Option<gdk::Cursor>,
+    focus_on_click: Option<bool>,
+    focusable: Option<bool>,
+    halign: Option<gtk::Align>,
+    has_tooltip: Option<bool>,
+    height_request: Option<i32>,
+    hexpand: Option<bool>,
+    hexpand_set: Option<bool>,
+    layout_manager: Option<gtk::LayoutManager>,
+    margin_bottom: Option<i32>,
+    margin_end: Option<i32>,
+    margin_start: Option<i32>,
+    margin_top: Option<i32>,
+    name: Option<String>,
+    opacity: Option<f64>,
+    overflow: Option<gtk::Overflow>,
+    receives_default: Option<bool>,
+    sensitive: Option<bool>,
+    tooltip_markup: Option<String>,
+    tooltip_text: Option<String>,
+    valign: Option<gtk::Align>,
+    vexpand: Option<bool>,
+    vexpand_set: Option<bool>,
+    visible: Option<bool>,
+    width_request: Option<i32>,
+    accessible_role: Option<gtk::AccessibleRole>,
+    #[property(bind = false, setter = false)]
+    _phantom: PhantomData<Children>,
+}
+
+impl<Children, S, M, R> View<S, M, R> for Notebook<Children>
+where
+    Children: ElementSeq<S, M, R>,
+    Children::Storage:
+        for<'a> Traversable<ReconcileChildrenVisitor<'a>, MessageContext<M>, (), S, M, R>,
+{
+    type Children = Children;
+
+    type State = gtk::Notebook;
+
+    fn lifecycle(
+        &self,
+        lifecycle: Lifecycle<Self>,
+        state: &mut Self::State,
+        children: &mut <Self::Children as ElementSeq<S, M, R>>::Storage,
+        context: &mut MessageContext<M>,
+        store: &Store<S>,
+        renderer: &mut R,
+    ) {
+        let is_static: bool = <Self::Children as ElementSeq<S, M, R>>::Storage::IS_STATIC;
+        let needs_reconcile = match lifecycle {
+            Lifecycle::Mount => true,
+            Lifecycle::Remount | Lifecycle::Unmount => !is_static,
+            Lifecycle::Update(old_view) => {
+                self.update(&old_view, state);
+                !is_static
+            }
+        };
+        if needs_reconcile {
+            let mut visitor = ReconcileChildrenVisitor::new(state);
+            children.for_each(&mut visitor, context, store, renderer);
+        }
+    }
+
+    fn build(
+        &self,
+        _children: &mut <Self::Children as ElementSeq<S, M, R>>::Storage,
+        _store: &Store<S>,
+        _renderer: &mut R,
+    ) -> Self::State {
+        self.build()
+    }
+}
+
+impl<'event, Children> EventListener<'event> for Notebook<Children> {
+    type Event = ();
+}
+
+#[derive(Debug, Clone)]
+pub enum NotebookChild<Child> {
+    Tab(Child),
+    Content(Child),
+}
+
+impl<Child> NotebookChild<Child> {
+    pub fn child(&self) -> &Child {
+        match self {
+            Self::Tab(child) => child,
+            Self::Content(child) => child,
+        }
+    }
+
+    pub fn into_child(self) -> Child {
+        match self {
+            Self::Tab(child) => child,
+            Self::Content(child) => child,
+        }
+    }
+}
+
+impl<Child, S, M, R> View<S, M, R> for NotebookChild<Child>
+where
+    Child: View<S, M, R>,
+{
+    type Children = Child::Children;
+
+    type State = Child::State;
+
+    fn lifecycle(
+        &self,
+        lifecycle: Lifecycle<Self>,
+        state: &mut Self::State,
+        children: &mut <Self::Children as ElementSeq<S, M, R>>::Storage,
+        context: &mut MessageContext<M>,
+        store: &Store<S>,
+        renderer: &mut R,
+    ) {
+        let lifecycle = lifecycle.map(|view| view.into_child());
+        self.child()
+            .lifecycle(lifecycle, state, children, context, store, renderer)
+    }
+
+    fn event(
+        &self,
+        event: <Self as EventListener>::Event,
+        state: &mut Self::State,
+        children: &mut <Self::Children as ElementSeq<S, M, R>>::Storage,
+        context: &mut MessageContext<M>,
+        store: &Store<S>,
+        renderer: &mut R,
+    ) {
+        self.child()
+            .event(event, state, children, context, store, renderer)
+    }
+
+    fn build(
+        &self,
+        children: &mut <Self::Children as ElementSeq<S, M, R>>::Storage,
+        store: &Store<S>,
+        renderer: &mut R,
+    ) -> Self::State {
+        self.child().build(children, store, renderer)
+    }
+}
+
+impl<'event, Child: EventListener<'event>> EventListener<'event> for NotebookChild<Child> {
+    type Event = Child::Event;
+}
+
+pub struct ReconcileChildrenVisitor<'a> {
+    container: &'a gtk::Notebook,
+    current_child: Option<gtk::Widget>,
+    current_tab: Option<gtk::Widget>,
+    index: u32,
+}
+
+impl<'a> ReconcileChildrenVisitor<'a> {
+    fn new(container: &'a gtk::Notebook) -> Self {
+        Self {
+            container,
+            current_child: container.first_child(),
+            current_tab: None,
+            index: 0,
+        }
+    }
+}
+
+impl<'a, V, CS, S, M, R> Visitor<ViewNode<NotebookChild<V>, CS, S, M, R>, S, R>
+    for ReconcileChildrenVisitor<'a>
+where
+    V: View<S, M, R>,
+    V::State: AsRef<gtk::Widget>,
+    CS: ComponentStack<S, M, R, View = NotebookChild<V>>,
+    NotebookChild<V>: View<S, M, R, Children = V::Children, State = V::State>,
+{
+    type Context = MessageContext<M>;
+
+    type Output = ();
+
+    fn visit(
+        &mut self,
+        node: &mut ViewNode<NotebookChild<V>, CS, S, M, R>,
+        _context: &mut MessageContext<M>,
+        _store: &Store<S>,
+        _renderer: &mut R,
+    ) -> Self::Output {
+        match node.state().as_view() {
+            NotebookChild::Tab(_) => {
+                let new_child: &gtk::Widget = node.state().as_view_state().unwrap().as_ref();
+                self.current_tab = Some(new_child.clone());
+            }
+            NotebookChild::Content(_) => {
+                let new_child: &gtk::Widget = node.state().as_view_state().unwrap().as_ref();
+                loop {
+                    match self.current_child.take() {
+                        Some(child) if new_child == &child => {
+                            self.current_child = child.next_sibling();
+                            self.index += 1;
+                            break;
+                        }
+                        Some(child) if new_child.parent().is_some() => {
+                            self.current_child = child.next_sibling();
+                            self.container.detach_tab(&child);
+                            self.index += 1;
+                        }
+                        Some(child) => {
+                            let new_tab = self.current_tab.take();
+                            self.container.insert_page(
+                                new_child,
+                                new_tab.as_ref(),
+                                Some(self.index),
+                            );
+                            new_child.insert_before(self.container, Some(&child));
+                            self.current_child = Some(child);
+                            self.index += 1;
+                            break;
+                        }
+                        None => {
+                            let new_tab = self.current_tab.take();
+                            self.container.append_page(new_child, new_tab.as_ref());
+                            self.index += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Drop for ReconcileChildrenVisitor<'a> {
+    fn drop(&mut self) {
+        while let Some(current_child) = self.current_child.take() {
+            self.container.detach_tab(&current_child);
+            self.current_child = current_child.next_sibling();
+        }
+    }
+}
